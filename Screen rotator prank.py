8830e5f081e92d3b51f56dc38e89fb606ba9a66e
Anvesh Katiyar <69871258+anvesh-27@users.noreply.github.com>
2021-07-04 21:56:59 +0530

    Add files via upload

---
+++ Screen rotator prank.py
@@ -0,0 +1,16 @@
+import time
+import rotatescreen
+
+screen = rotatescreen.get_primary_display()
+
+def screenRotate(axis):
+    screen.rotate_to(axis)
+    time.sleep(10)
+
+axis = 0
+
+while True:
+    axis = axis + 90
+    if axis > 270:
+        axis = 90
+    screenRotate(axis)
